aedef3e1d4a9164a7c094e24b1f64d6bdffb27e8
umeshbelge <supriyasukale11@gmail.com>
2024-12-08 09:15:28 +0530

    added ./

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="./">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
